bdaec21e422ad1669dcbd97a79f2dbfe87be8e14
Harry <68599167+harrykeeran12@users.noreply.github.com>
2025-04-02 19:28:36 +0100

    fix: added confusion matrix script.

--- tests/test_evaluateData.py
+++ tests/test_evaluateData.py
@@ -1,6 +1,8 @@
 import unittest
 from pathlib import Path
-
+from evaluateData import JSONtoErrorArray, cleanJSON
+import pandas as pd
+from schema import RadiologyErrors
 # To run the tests, use python3 -m unittest tests/<name of this py file>.py
 
 
@@ -17,6 +19,55 @@ class TestEvaluateData(unittest.TestCase):
         self.assertTrue(
             Path.exists(Path("datasets/annotated_testing_data.csv")),
             "Please check the GitHub repository or the provided dissertation for the data. Alternatively, contact the owner by email.",
+        )
+
+    def test_annotated_errorNum(self):
+        df = pd.read_csv("datasets/annotated_testing_data.csv")
+        df["Decoded"] = df["Reference JSON Output"].apply(
+            lambda x: RadiologyErrors.model_validate_json(cleanJSON(x))
+        )
+        df["Error Array"] = df["Decoded"].apply(lambda s: JSONtoErrorArray(s))
+
+        df["Counted I"] = df["Error Array"].apply(
+            lambda ea: ea["Internal Inconsistency"]
+        )
+        df["Counted O"] = df["Error Array"].apply(lambda ea: ea["Omission"])
+        df["Counted T"] = df["Error Array"].apply(lambda ea: ea["Transcription Error"])
+        df["Counted E"] = df["Error Array"].apply(lambda ea: ea["Extraneous Statement"])
+        print("Results in Reference Data vs Counted Reference Data Results: ")
+
+        # print(
+        #     f"Omissions: {sum(df['Omission'])}: Number of counted omissions in testing data: {sum(df['Counted O'])}"
+        # )
+
+        # print(
+        #     f"Internal Inconsistency: {sum(df['Internal Inconsistency'])}Number of counted internal inconsistencies in testing data: {sum(df['Counted I'])}"
+        # )
+        # print(
+        #     f"Extraneous Statements: {sum(df['Extraneous Statement'])} Number of counted extraneous statements in testing data: {sum(df['Counted E'])}"
+        # )
+        # print(
+        #     f"Transcription Errors: {sum(df['Transcription Error'])}Number of counted transcription errors in testing data: {sum(df['Counted T'])}"
+        # )
+        self.assertEqual(
+            sum(df["Omission"]),
+            sum(df["Counted O"]),
+            msg="The number of omission errors reported are inconsistent with the JSON.",
+        )
+        self.assertEqual(
+            sum(df["Internal Inconsistency"]),
+            sum(df["Counted I"]),
+            msg="The number of internal inconsistency errors reported are inconsistent with the JSON.",
+        )
+        self.assertEqual(
+            sum(df["Extraneous Statement"]),
+            sum(df["Counted E"]),
+            msg="The number of extraneous statements errors reported are inconsistent with the JSON.",
+        )
+        self.assertEqual(
+            sum(df["Transcription Error"]),
+            sum(df["Counted T"]),
+            msg="The number of transcription errors reported are inconsistent with the JSON.",
         )
 
     def test_prompt1_mistral(self):
